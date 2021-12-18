18847988e189f7a5bcf975b9fc68d17a543dd6d4
Faiyaz Vora <faiyazvora@gmail.com>
2021-12-18 21:59:10 +0530

    Route Changes
    Route Change

--- College Management System/App_Start/RouteConfig.cs
+++ College Management System/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace College_Management_System
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "College", action = "AddStud", id = UrlParameter.Optional }
             );
         }
     }
